eb194b77764e943c362af4ce577adb7384f4f279
andrew.everall1995@gmail.com <andrew.everall1995@gmail.com>
2020-08-02 11:17:04 +0100

    Removing gaps code included.

--- scanninglaw/cog_i.py
+++ scanninglaw/cog_i.py
@@ -47,7 +47,7 @@ class dr2_sl(ScanningLaw):
     Queries the Gaia DR2 selection function (Boubert & Everall, 2019).
     """
 
-    def __init__(self, map_fname=None, version='cogi_2020', test=False):
+    def __init__(self, map_fname=None, version='cogi_2020', sample='Astrometry', test=False):
         """
         Args:
             map_fname (Optional[:obj:`str`]): Filename of the Boubert,Everall,Holl 2020 scanning law. Defaults to
@@ -56,10 +56,16 @@ class dr2_sl(ScanningLaw):
                 are :obj:`'cogi_2020'`
                 are :obj:`'dr2_nominal'`
                 Defaults to :obj:`'cogi_2020'`.
+            sample (Optional[:obj:`str`]): The Gaia data sample being used. Valid options are
+                are :obj:`'Astrometry'`
+                are :obj:`'Photometry'`
+                Defaults to :obj:`'Spectroscopy'`.
         """
 
         if map_fname is None:
             map_fname = os.path.join(data_dir(), 'cog', '{}.csv'.format(version))
+
+        gaps_fname = os.path.join(data_dir(), 'cog', '{}.csv'.format(sample))
 
         t_start = time()
 
@@ -72,9 +78,16 @@ class dr2_sl(ScanningLaw):
         else: _data = pd.read_csv(map_fname, usecols=_columns)
         _keys = ['tcb_at_gaia','tcb_at_bary1','tcb_at_bary2','ra_fov_1','dec_fov_1','angle_fov_1','ra_fov_2','dec_fov_2','angle_fov_2']
         _box = {}
-        for j,k in zip(_columns,_keys):
-            _box[k] = _data[j].values
+        for j,k in zip(_columns,_keys): _box[k] = _data[j].values
         _box['scan_idx'] = np.arange(len(_box['ra_fov_1']))
+
+        ##### Load gaps
+        _columns = ['start [rev]', 'end [rev]'];
+        _data = pd.read_csv(gaps_fname, usecols=_columns)
+        self._gaps = np.vstack((_data['start [rev]'].values, _data['end [rev]'].values)).T
+        if sample=='Astrometry':
+            self._gaps = np.vstack((np.array([-np.inf, 1694.8761])[np.newaxis,:], self._gaps ))
+            self._gaps = np.vstack(( self._gaps, np.array([2334.4836, np.inf])[np.newaxis,:],  ))
 
         t_auxilliary = time()
 
@@ -176,13 +189,23 @@ class dr2_sl(ScanningLaw):
             _valid = np.array(_in_fov,dtype=np.int)[_where]
             n_fov1 = np.sum(condition[:n_fov1]);
 
-            t_previous[_valid] = _t_now
-
-            if len(_valid)>0:
+            #if len(_valid)>0:
+            if np.sum(condition)>0:
                 tcbgaia_fov1 = self.linearbisect(_xyz[_where[:n_fov1]], self.xyz_fov_1[max(0,_tidx-1):_tidx+2][:2][:,np.newaxis],
                                                                         self.tcb_at_gaia[max(0,_tidx-1):_tidx+2][:2][:,np.newaxis])
                 tcbgaia_fov2 = self.linearbisect(_xyz[_where[n_fov1:]], self.xyz_fov_2[max(0,_tidx-1):_tidx+2][:2][:,np.newaxis],
                                                                         self.tcb_at_gaia[max(0,_tidx-1):_tidx+2][:2][:,np.newaxis])
+
+                tcbgaia = np.hstack((tcbgaia_fov1, tcbgaia_fov2))
+                condition_gap = np.ones(len(tcbgaia)).astype(bool)
+                for ii in range(self._gaps.shape[0]):
+                    condition_gap = condition_gap&( (tcbgaia<self._gaps[ii,0])|(tcbgaia>self._gaps[ii,1]) )
+
+                _valid = _valid[condition_gap]
+                n_fov1 = np.sum(condition_gap[:n_fov1]);
+
+                t_previous[_valid] = _t_now
+
                 for ii in range(n_fov1):
                     _sidx = _valid[ii]
                     tgaia_fov1[_sidx].append(tcbgaia_fov1[ii])
@@ -242,18 +265,31 @@ class dr2_sl(ScanningLaw):
                 _tidx_fov1 = np.vstack((_in_fov[:n_fov1][condition_fov1]-1, _in_fov[:n_fov1][condition_fov1]))
                 _tidx_fov1.T[_tidx_fov1[0]<0] = np.array([0,1])
                 tcbgaia_fov1 = self.linearbisect(_xyz, self.xyz_fov_1[_tidx_fov1], self.tcb_at_gaia[_tidx_fov1])
-                tgaia_fov1[_sidx] = tcbgaia_fov1
-                nscan_fov1[_sidx] = np.sum(condition_fov1)
+
+                # Apply scanning law gaps
+                condition_gap1 = np.ones(np.sum(condition_fov1)).astype(bool)
+                for ii in range(self._gaps.shape[0]):
+                    condition_gap1 = condition_gap1&( (tcbgaia_fov1<self._gaps[ii,0])|(tcbgaia_fov1>self._gaps[ii,1]) )
+
+                tgaia_fov1[_sidx] = list(tcbgaia_fov1[condition_gap1])
+                nscan_fov1[_sidx] = np.sum(condition_gap1)
 
             if n_fov2>0:
+
                 condition_fov2 = self.tcb_at_gaia[_in_fov[n_fov1:]][1:] - self.tcb_at_gaia[_in_fov[n_fov1:]][:-1] > self.t_diff
                 condition_fov2 = np.insert(condition_fov2, 0, True)
 
                 _tidx_fov2 = np.vstack((_in_fov[n_fov1:][condition_fov2]-1, _in_fov[n_fov1:][condition_fov2]))
                 _tidx_fov2.T[_tidx_fov2[0]<0] = np.array([0,1])
                 tcbgaia_fov2 = self.linearbisect(_xyz, self.xyz_fov_2[_tidx_fov2], self.tcb_at_gaia[_tidx_fov2])
-                tgaia_fov2[_sidx] = tcbgaia_fov2
-                nscan_fov2[_sidx] = np.sum(condition_fov2)
+
+                # # Apply scanning law gaps
+                condition_gap2 = np.ones(np.sum(condition_fov2)).astype(bool)
+                for ii in range(self._gaps.shape[0]):
+                    condition_gap2 = condition_gap2&( (tcbgaia_fov2<self._gaps[ii,0])|(tcbgaia_fov2>self._gaps[ii,1]) )
+
+                tgaia_fov2[_sidx] = list(tcbgaia_fov2[condition_gap2])
+                nscan_fov2[_sidx] = np.sum(condition_gap2)
 
         return tgaia_fov1, tgaia_fov2, nscan_fov1, nscan_fov2
 
@@ -291,7 +327,10 @@ class dr2_sl(ScanningLaw):
               tgaia_fov1, tgaia_fov2, nscan_fov1, nscan_fov2 = self._scanning_law(xyz_source)
         else: tgaia_fov1, tgaia_fov2, nscan_fov1, nscan_fov2 = self._scanning_law_inverse(xyz_source)
 
-        return {'tgaia_fov1':tgaia_fov1, 'tgaia_fov2':tgaia_fov2, 'nscan_fov1':nscan_fov1, 'nscan_fov2':nscan_fov2, 'shape':coord_shape}
+        tgaia_fov1 = np.array(tgaia_fov1).reshape(coord_shape)
+        tgaia_fov2 = np.array(tgaia_fov2).reshape(coord_shape)
+
+        return tgaia_fov1, tgaia_fov2, nscan_fov1, nscan_fov2
 
 
 def fetch(version='cogi_2020', fname=None):
